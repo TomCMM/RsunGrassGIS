905d763f9647dff53a2970dfab85e063798e8f6b
Thomas Christian <martinthomas.2112@gmail.com>
2015-12-07 14:34:24 -0200

    Add anargument to save the global irradiance in the grasscode.py

--- grassCode.py
+++ grassCode.py
@@ -46,10 +46,12 @@ class IrradiationRsun():
  
         grass.run_command("r.slope.aspect", overwrite=True, elevation=self.rasterelev, aspect=aspect, slope=slope)
  
-    def irradiance(self,time,day, alb=0.2, lin=3, flat = None):
+    def irradiance(self,time,day, alb=0.2, lin=3, flat = None, outpath=None):
         """
         DESCRIPTION
             Calculate the Irradiance
+        INPUT
+            Outpath: True, use the path to save the global irradiance array on a file
         """
  
         try:
@@ -91,6 +93,14 @@ class IrradiationRsun():
         self.rasterdiff = "diff_rad"+ "@" + self.env.get('MAPSET')# complete name
         self.rasterrefl = "refl_rad"+ "@" + self.env.get('MAPSET')# complete name
         self.rasterglob = "glob_rad"+ "@" + self.env.get('MAPSET')# complete name
+        
+        if outpath:
+            irrglob = garray.array()
+            irrglob.read(self.rasterelev)
+            filename = outpath + str(day) + "_" + str(time)
+            np.savetxt(filename, irrglob, delimiter=',') 
+            print "Saved at :" + outpath
+            
  
     def get_values(self, mapname, east, north):
         """
@@ -206,6 +216,9 @@ class IrradiationRsun():
         INPUT
             years: just to put a year at the index datetime 
             then the data are easily comparable to the observations
+            lag: apply a lag to the measure
+            lin: Linketurbidity factor for the calcul of the divergence
+            years: years to perform the simulation
         """
 
         if not isinstance(east,list):
@@ -246,7 +259,7 @@ class IrradiationRsun():
             print day
             for hour in hours:
                 time = np.array(hour+lag)
-                self.irradiance(day=day, time=time, flat = flat, lin=lin)
+                self.irradiance(day=day, time=time, flat = flat, lin=lin) # Compute the irradiance
                 days_column.append(day)
                 hours_column.append(hour)
 
@@ -317,69 +330,86 @@ class IrradiationRsun():
 
 if __name__ == '__main__':
     namerasterelev = "ASTGTM2_S23W047_dem_crop"
-# C4
-# C5
-# C6
-# C7
-# C8
-# C9
-# C10
-# C11
-# C12
-# C13
-# C14
-# C15
-# C16
-# C17
-# C18
-# C19
 
-    north = [
-            -22.88097,
-            -22.88117,
-            -22.87792,
-            -22.87686,
-            -22.87411,
-            -22.87019,
-            -22.88331,
-            -22.88344,
-            -22.88628,
-            -22.88839,
-            -22.88914,
-            -22.88964,
-            -22.86303,
-            -22.86478,
-            -22.86414,
-            -22.86439
-            ]
- 
-    east = [
-            -46.249083,
-            -46.251667,
-            -46.252861,
-            -46.254528,
-            -46.256667,
-            -46.258833,
-            -46.246944,
-            -46.245861,
-            -46.243694,
-            -46.241278,
-            -46.238472,
-            -46.237139,
-            -46.24731,
-            -46.24394,
-            -46.23861,
-            -46.23614
-             ]
     irr = IrradiationRsun(namerasterelev)
     irr.display_region()
     irr.display_env()
-
-    r_sunlags = [-0.2]
-    lins = [2]
+    
     hours = np.arange(5,21)
-    days = range(1,355)
-    for rsun_lag in  r_sunlags:
-        for lin in lins:
-            df = irr.get_serie_irradiance(days, hours, east=east, north=north, lag = rsun_lag, lin=lin, outpath='/home/thomas')
+    days = range(1,2)
+    
+    for day in days:
+        for hour in hours:
+            irr.irradiance(hour, day, 2, 2, flat=None, outpath="/home/thomas/")
+
+
+
+#===============================================================================
+# Get a serie of value
+#===============================================================================
+# # C4
+# # C5
+# # C6
+# # C7
+# # C8
+# # C9
+# # C10
+# # C11
+# # C12
+# # C13
+# # C14
+# # C15
+# # C16
+# # C17
+# # C18
+# # C19
+# 
+#     north = [
+#             -22.88097,
+#             -22.88117,
+#             -22.87792,
+#             -22.87686,
+#             -22.87411,
+#             -22.87019,
+#             -22.88331,
+#             -22.88344,
+#             -22.88628,
+#             -22.88839,
+#             -22.88914,
+#             -22.88964,
+#             -22.86303,
+#             -22.86478,
+#             -22.86414,
+#             -22.86439
+#             ]
+#  
+#     east = [
+#             -46.249083,
+#             -46.251667,
+#             -46.252861,
+#             -46.254528,
+#             -46.256667,
+#             -46.258833,
+#             -46.246944,
+#             -46.245861,
+#             -46.243694,
+#             -46.241278,
+#             -46.238472,
+#             -46.237139,
+#             -46.24731,
+#             -46.24394,
+#             -46.23861,
+#             -46.23614
+#              ]
+#     irr = IrradiationRsun(namerasterelev)
+#     irr.display_region()
+#     irr.display_env()
+# 
+#     r_sunlags = [-0.2]
+#     lins = [2]
+#     hours = np.arange(5,21)
+#     days = range(1,355)
+#     for rsun_lag in  r_sunlags:
+#         for lin in lins:
+#             df = irr.get_serie_irradiance(days, hours, east=east, north=north, lag = rsun_lag, lin=lin, outpath='/home/thomas')
 
